5c6655b551b65f4db2af010ce3e1db9a8b3e7ab6
ahstunetwork <54517633+ahstunetwork@users.noreply.github.com>
2023-03-06 22:22:19 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "src/Timestamp.h"
+
+
+using namespace std;
+
+// todo task below
+
+
+int main() {
+    Vita::Timestamp time;
+    cout << time.now().to_string() << endl;
+
+    return 0;
+}
+
